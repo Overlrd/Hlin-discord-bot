2b366f94596f7f8be76baab22520bbf199fb0655
Overlrd <premieraugust28@gmail.com>
2023-01-27 16:17:53 +0000

    added add_quote option

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-import os
-
-from dotenv import load_dotenv
-load_dotenv()
-
-TOKEN = os.environ['TOKEN']
 import discord 
-import random
-# request module to get data from api
+from discord.ext import commands 
+import os 
 import requests
-#json to read the api response
 import json
-from replit import db
-intents = discord.Intents.default()
-intents.message_content = True
+from discord import app_commands
+from pymongo import MongoClient
+from datetime import datetime
 
+import pprint
+# import db 
 
+# API URL
 API_URL = 'https://zenquotes.io/api/random'
 
 
-#keep_alive
-from keep_alive import keep_alive
+## set up the bot 
+TOKEN = os.environ['TOKEN']
+bot = commands.Bot(command_prefix="/", intents = discord.Intents.all())
+
+## set up  mongo db 
+MONGO_CONN_LINK = my_secret = os.environ['CONN_LINK']
+Client_Mongo = MongoClient(MONGO_CONN_LINK)
+
+quotes_db = Client_Mongo.quotes
+quotes_collection = quotes_db.quotes_collection
+
+res_mongo = quotes_collection.find()
+printer = pprint.PrettyPrinter()
+
+for i in res_mongo:
+  printer.pprint(i)
 
 
-sad_words = ['sad', 'angry','unhappy']
-
-starter_encouragements = [
-  'Cheer up',
-  'Hang on ',
-  'Keep it '
-]
-
-if "responding" not in db.keys():
-  db['responding'] = True 
-
+## get quotes 
 def get_quote():
   #make a get request to the zen quotes api
   response = requests.get(API_URL)
@@ -43,94 +43,52 @@ def get_quote():
   quote = response_json[0]['q'] + " -" + response_json[0]['a']
   return quote
 
-def update_encouragements(encouraging_message):
-  if 'encouragements' in db.keys():
-    encouragements = db['encouragements']
-    encouragements.append(encouraging_message)
-    db['encouragements'] = encouragements
-  else:
-    db['encouragements'] = [encouraging_message]
 
 
-def  delete_encouragement(index):
-  encouragements = db['encouragements']
-  if len(encouragements) > index:
-    del encouragements[index]
-    db['encouragements'] = encouragements
+
+
+def post_quote(quote_text, quote_author):
+  now = datetime.now().strftime("%y:%m:%d:%H:%M:%S")
+  #quote_author = quote_author.split("@", 1)[1]
+  doc = {"author":quote_author, "quote":quote_text, "date" : now}
+  try :
+    quotes_collection.insert_one(doc)
+    print("quote sended ")
+  except Exception as e:
+    print(e)
   
 
 
 
 
+@bot.event 
+async def on_ready():
+    print(f'--------------------Logged--------------')
+
+    try:
+        synced = await bot.tree.sync()
+        print(f" {len(synced)} commands synced " )
+    except Exception as e :
+      print(e)
+      
+  
+      
+## main func 
+
+      
+@bot.tree.command(name = "inspire")
+async def inspire(interaction : discord.Interaction):
+    quote = get_quote()
+    await interaction.response.send_message(quote, ephemeral = True )
 
 
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
+@bot.tree.command(name = "add_quote")
+async def add_quote(interaction : discord.Interaction , quote: str):
+  await interaction.response.send_message(f"{quote} by  {interaction.user.mention} ", ephemeral = True)
+  print(interaction.user)
+  post_quote(quote, str(interaction.user))
+    
+    
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-          return
-
-
-        msg = message.content
-
-        if msg.startswith('!inspire'):
-          async with message.channel.typing():
-            quote = get_quote()
-          await message.reply(quote, mention_author=True)
-
-        if db['responding'] :
-          options = starter_encouragements
-          if 'encouragements' in db.keys():
-            options = options + db['encouragements'].value
-        
-          if any(word in msg for word in sad_words):
-            await message.reply(random.choice(options),  mention_author=True)
-
-        if msg.startswith('!new'):
-          encouraging_msg = msg.split('!new ', 1)[1]
-          update_encouragements(encouraging_msg)
-          await message.reply(f'new quote quote added', mention_author=True)
-
-
-        if msg.startswith('!del'):
-          encouragements = []
-          if 'encouragements' in db.keys():
-            index = int(msg.split('!del', 1)[1])
-            delete_encouragement(index)
-            encouragements = db['encouragements']
-            await message.reply(f'encouagement removed ')
-
-
-        if msg.startswith('!list'):
-          encouragements = []
-          if 'encouragements' in db.keys():
-            encouragements = db['encouragements']
-          await message.reply(f'List :{encouragements}', mention_author = True)
-
-
-        if msg.startswith('!responding'):
-          value = msg.split('!responding ', 1)[1]
-          if value.lower() == "true":
-            db['responding'] = True 
-            await message.reply('Bot response is on', mention_author = True)
-          else:
-            db['responding'] = False 
-            await message.reply(f'Bot response is off', mention_author = True)
-            
-        
-
-
-
-
-def main():
-  keep_alive()
-  client = MyClient(intents=intents)
-  client.run(TOKEN)
-
-
-if __name__ == '__main__':
-  main()
+bot.run(TOKEN)

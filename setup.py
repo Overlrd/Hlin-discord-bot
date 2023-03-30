c2f2a3eb11d2bc7c2a6e6e388a23919ad9650414
Overlrd <premieraugust28@gmail.com>
2023-03-30 16:01:01 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -12,12 +12,16 @@ message = "--------------SETUP - MAKE SURE TO CHECK THE README------------------
 print(f"{Fore.GREEN}{Style.BRIGHT}{message}{Style.RESET_ALL}")
 
 # Define the variables to be written to the .env file
-DISCORD_BOT_TOKEN = input("YOUR DISCORD API KEY : ")
-MONGODB_CONN_LINK = input("YOUR MONGODB CONN LINK : ")
-GOOGLE_PERSPECTIVE_KEY = input("YOUR GOOGLE PERSPECTIVE API KEY : ")
-GOOGLE_CUSTOM_SEARCH_KEY = input("YOUR GOOGLE CUSTOM SEARCH KEY : ")
-GOOGLE_CUSTOM_SEARCH_ENGINE_ID = input("YOUR CUSTOM SEARCH ENGINE ID KEY : ")
+try :
 
+    DISCORD_BOT_TOKEN = input("YOUR DISCORD API KEY : ")
+    MONGODB_CONN_LINK = input("YOUR MONGODB CONN LINK : ")
+    GOOGLE_PERSPECTIVE_KEY = input("YOUR GOOGLE PERSPECTIVE API KEY : ")
+    GOOGLE_CUSTOM_SEARCH_KEY = input("YOUR GOOGLE CUSTOM SEARCH KEY : ")
+    GOOGLE_CUSTOM_SEARCH_ENGINE_ID = input("YOUR CUSTOM SEARCH ENGINE ID KEY : ")
+except Exception as e :
+    print(e)
+    
 # Create the .env file and write the variables to it
 def create_env_and_vars(bot_token , mongodb_link , perspective_key, custom_search, custom_engine):
     logging.info("writing env variables")
@@ -38,7 +42,11 @@ def create_config_file(config_file_name="config.json"):
         json.dump(config_data, f, indent=4)
 
 try :
-    create_env_and_vars(DISCORD_BOT_TOKEN, MONGODB_CONN_LINK, GOOGLE_PERSPECTIVE_KEY)
+    create_env_and_vars(DISCORD_BOT_TOKEN, 
+                        MONGODB_CONN_LINK, 
+                        GOOGLE_PERSPECTIVE_KEY,
+                        GOOGLE_CUSTOM_SEARCH_KEY,
+                        GOOGLE_CUSTOM_SEARCH_ENGINE_ID)
     create_config_file()
     print(f"{Fore.GREEN} SETUP COMPLETED - RUN 'python3 main.py' TO START THE BOT {Style.RESET_ALL}")
 except Exception as e :
